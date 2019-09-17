a9033db729dae27125da20349bcadce3f9e02cbe
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-09-17 12:40:02 -0800

    backup

--- get_leg_staff.py
+++ get_leg_staff.py
@@ -2,10 +2,10 @@ import os
 import sys
 
 def get_email(first, last):
-  return first + '.' + last + 'akleg.gov'
+  return first + '.' + last + '@akleg.gov'
 
 def leg_to_key(leg):
-  return leg[1] + leg[0]
+  return leg[0] + ',' + leg[1]
 
 def cleanup(line):
   first_chunk = ''
@@ -29,6 +29,32 @@ def dots_to_comma(line):
   joined_line = '.'.join(cleaned_line) 
   return cleanup(joined_line)
 
+def print_rep_file(leg_list):
+  handle = open('/Users/nknapp/Desktop/akpirg/reps_contact.txt', 'w')
+  handle.write('First,Last,Email,Phone\n')
+  for leg in leg_list:
+    if 'Rep.' in leg[2]:
+      handle.write(','.join(leg) + '\n')
+
+def print_sen_file(leg_list):
+  handle = open('/Users/nknapp/Desktop/akpirg/sens_contact.txt', 'w')
+  handle.write('First,Last,Email,Phone\n')
+  for leg in leg_list:
+    if 'Sen.' in leg[2]:
+      handle.write(','.join(leg) + '\n')
+
+def print_staff_file(staff_dict):
+  handle = open('/Users/nknapp/Desktop/akpirg/staff_contact.txt', 'w')
+  handle.write('First,Last,Email,Legislator First,Legislator Last\n')
+  for legislator in staff_dict:
+    for staff in staff_dict[legislator]:
+      line_info = []
+      line_info.append(staff[0])
+      line_info.append(staff[1])
+      line_info.append(get_email(staff[0], staff[1]))
+      line_info.append(legislator)
+      handle.write(','.join(line_info) + '\n')
+
 bad_file = '/Users/nknapp/Desktop/akpirg/leg_staff.txt'
 handle = open(bad_file, 'r')
 lines = handle.readlines()
@@ -41,14 +67,20 @@ for line in lines:
     leg_name = dot_split_line[0].lower()
     leg_last_name = leg_name.split(' ')[0][:-1].title()
     leg_first_name = leg_name.split(' ')[1].title()
-    leg_list.append([leg_first_name, leg_last_name])
     full_staff_list = line[:-1].split('Staff:')[-1].split(',')
-    leg_key = leg_last_name + leg_first_name
+    leg_key = leg_first_name + ',' + leg_last_name
     staff_dict[leg_key] = []
+    cleaned_line = dots_to_comma(line)
+    comma_split_line = cleaned_line.split(',')
+    email = comma_split_line[2]
+    phone = comma_split_line[3]  
     for staff in full_staff_list:
       first = staff.split(' ')[1] 
       last = staff.split(' ')[2] 
-      staff_dict[leg_key].append([first, last])
+      staff_dict[leg_key].append([first, last, phone])
+    leg_list.append([leg_first_name, leg_last_name, email, phone])
 
-for line in lines:
-  print(dots_to_comma(line))
+#print_rep_file(leg_list)
+#print_sen_file(leg_list)
+
+print_staff_file(staff_dict)    
