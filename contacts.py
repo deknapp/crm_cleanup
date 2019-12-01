c1b6a2159fe91166858b738b2e4f6992437875b6
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-11-30 21:24:01 -0900

    backup

--- contacts.py
+++ contacts.py
@@ -14,12 +14,8 @@ def contact_dict(record_name):
   lines = record_handle.readlines()
   header = lines[0].split(' ') 
   contact_dict = {}
-  print(len(header))
   for line in lines[1:]:
     split_line = line.split('|')
-    print(len(split_line))
-    for i in range(len(header)-1):
-      print(header[i], split_line[i])
     contact_dict[split_line[0]] = {}
     for i in range(1, len(header)-1):
       contact_dict[split_line[0]][header[i]] = split_line[i]
--- voter_file.py
+++ voter_file.py
@@ -7,14 +7,26 @@ def read_voter_file(voter_file_name):
   fle = open(voter_file_name, 'r')
   return csv.DictReader(fle, quotechar='|')
 
+def name_contact_dict(contact_dict):
+  name_dict = dict()
+  for key, val in contact_dict:
+    k = val['First'] + val['Last']
+    name_dict[k] = val
+  return name_dict
+
+def print_out
+
 voter_name = '/Users/nknapp/Desktop/akpirg/voter_file.csv'
 contact_name = '/Users/nknapp/Desktop/akpirg/state_voices.txt'
  
 voter_dict = read_voter_file(voter_name)
 contact_dict = contacts.contact_dict(contact_name)
+name_contact_dict = name_contact_dict(contact_dict)
 
 voter_list = list(voter_dict)
 
-print(voter_list[1:3])
-print(contact_dict)
- 
+for voter in voter_list:
+  print(voter)
+  break
+
+print(contact_dict[list(contact_dict.keys())[0]]) 
