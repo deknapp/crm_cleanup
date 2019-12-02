c715503134c3fe213468dd50747053033292b0a6
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-12-02 12:09:03 -0900

    fix some bugs in voter file

--- voter_file.py
+++ voter_file.py
@@ -9,14 +9,18 @@ def read_voter_file(voter_file_name):
 
 def name_contact_dict(contact_dict):
   name_dict = dict()
-  for key, val in contact_dict:
+  for key, val in contact_dict.items():
     k = val['First'] + val['Last']
     name_dict[k] = val
   return name_dict
 
 def state_voices_line(voter, contact):
   # TODO: figure out best way to format this from EveryAction site
+  
   return line 
+
+#def needs_correction(voter, contact):
+
 
 def correct_contact_list(voter_dict, contact_dict, contact_correction_file):
   corrected_contacts = open(contact_correction_file, 'r')
@@ -26,7 +30,8 @@ def correct_contact_list(voter_dict, contact_dict, contact_correction_file):
     key = first + last
     if key in contact_dict.keys():
       contact = contact_dict[key]
-      correct_contacts.write(state_voices_line(voter, contact)) 
+      if needs_correction(voter, contact):
+        correct_contacts.write(state_voices_line(voter, contact)) 
 
 voter_name = '/Users/nknapp/Desktop/akpirg/voter_file.csv'
 contact_name = '/Users/nknapp/Desktop/akpirg/state_voices.txt'
